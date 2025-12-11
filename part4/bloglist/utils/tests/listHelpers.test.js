b269db3a46ecf8963bc2164f7311435a601ff02f
Rowland Momoh <momohrowland@gmail.com>
2025-12-11 06:40:22 +0100

    Add ex.4.6 - 4.7 (advanced lodash helper function tests)

--- part4/bloglist/utils/tests/listHelpers.test.js
+++ part4/bloglist/utils/tests/listHelpers.test.js
@@ -1,9 +1,12 @@
 const { test, describe } = require('node:test')
 const assert = require('node:assert')
+const blogList = require('./mockData')
 const {
   dummy,
   calculateTotalLikes,
   calculateFaveBlog,
+  mostBlogs,
+  mostLikes,
 } = require('../listHelpers')
 
 test('dummy returns one', () => {
@@ -19,7 +22,7 @@ describe('total likes', () => {
   })
 
   test('of single list is the like of that', () => {
-    const blogList = [
+    const singleBlog = [
       {
         _id: '5a422aa71b54a676234d17f8',
         title: 'Go To Statement Considered Harmful',
@@ -29,118 +32,17 @@ describe('total likes', () => {
         __v: 0,
       },
     ]
-    assert.strictEqual(calculateTotalLikes(blogList), 5)
+    assert.strictEqual(calculateTotalLikes(singleBlog), 5)
   })
 
   test('of multiple list item is calculated right', () => {
-    const blogList = [
-      {
-        _id: '5a422a851b54a676234d17f7',
-        title: 'React patterns',
-        author: 'Michael Chan',
-        url: 'https://reactpatterns.com/',
-        likes: 7,
-        __v: 0,
-      },
-      {
-        _id: '5a422aa71b54a676234d17f8',
-        title: 'Go To Statement Considered Harmful',
-        author: 'Edsger W. Dijkstra',
-        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-        likes: 5,
-        __v: 0,
-      },
-      {
-        _id: '5a422b3a1b54a676234d17f9',
-        title: 'Canonical string reduction',
-        author: 'Edsger W. Dijkstra',
-        url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-        likes: 12,
-        __v: 0,
-      },
-      {
-        _id: '5a422b891b54a676234d17fa',
-        title: 'First class tests',
-        author: 'Robert C. Martin',
-        url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-        likes: 10,
-        __v: 0,
-      },
-      {
-        _id: '5a422ba71b54a676234d17fb',
-        title: 'TDD harms architecture',
-        author: 'Robert C. Martin',
-        url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-        likes: 0,
-        __v: 0,
-      },
-      {
-        _id: '5a422bc61b54a676234d17fc',
-        title: 'Type wars',
-        author: 'Robert C. Martin',
-        url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-        likes: 2,
-        __v: 0,
-      },
-    ]
     assert.deepStrictEqual(calculateTotalLikes(blogList), 36)
   })
 })
 
 describe('favourite blog', () => {
   test('with most like count', () => {
-    const blogList = [
-      {
-        _id: '5a422a851b54a676234d17f7',
-        title: 'React patterns',
-        author: 'Michael Chan',
-        url: 'https://reactpatterns.com/',
-        likes: 7,
-        __v: 0,
-      },
-      {
-        _id: '5a422aa71b54a676234d17f8',
-        title: 'Go To Statement Considered Harmful',
-        author: 'Edsger W. Dijkstra',
-        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-        likes: 5,
-        __v: 0,
-      },
-      {
-        _id: '5a422b3a1b54a676234d17f9',
-        title: 'Canonical string reduction',
-        author: 'Edsger W. Dijkstra',
-        url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-        likes: 12,
-        __v: 0,
-      },
-      {
-        _id: '5a422b891b54a676234d17fa',
-        title: 'First class tests',
-        author: 'Robert C. Martin',
-        url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-        likes: 10,
-        __v: 0,
-      },
-      {
-        _id: '5a422ba71b54a676234d17fb',
-        title: 'TDD harms architecture',
-        author: 'Robert C. Martin',
-        url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-        likes: 0,
-        __v: 0,
-      },
-      {
-        _id: '5a422bc61b54a676234d17fc',
-        title: 'Type wars',
-        author: 'Robert C. Martin',
-        url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-        likes: 2,
-        __v: 0,
-      },
-    ]
-
-    const faveBlog = {
+    const expectedResult = {
       _id: '5a422b3a1b54a676234d17f9',
       title: 'Canonical string reduction',
       author: 'Edsger W. Dijkstra',
@@ -149,6 +51,24 @@ describe('favourite blog', () => {
       __v: 0,
     }
 
-    assert.deepStrictEqual(calculateFaveBlog(blogList), faveBlog)
+    assert.deepStrictEqual(calculateFaveBlog(blogList), expectedResult)
+  })
+})
+
+describe('most blogs/likes', () => {
+  test('returns correct author name and blog count', () => {
+    const expectedResult = {
+      author: 'Robert C. Martin',
+      blogs: 3,
+    }
+    assert.deepStrictEqual(mostBlogs(blogList), expectedResult)
+  })
+
+  test('returns correct author name and like count', () => {
+    const expectedResult = {
+      author: 'Edsger W. Dijkstra',
+      likes: 17,
+    }
+    assert.deepStrictEqual(mostLikes(blogList), expectedResult)
   })
 })
