a735b8df7eec72958b10d92166a0fe72d6d20bf0
Rowland Momoh <momohrowland@gmail.com>
2026-01-15 21:25:43 +0100

    Add ex. 9.15 (course-info-ts)

---
+++ part9/courseinfo-ts/src/App.tsx
@@ -0,0 +1,57 @@
+interface CoursePart {
+  name: string;
+  exerciseCount: number;
+}
+
+const Header = ({ name }: { name: string }) => {
+  return <h1>{name}</h1>;
+};
+
+const Content = ({ parts }: { parts: CoursePart[] }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <p key={part.name}>
+          {part.name} {part.exerciseCount}
+        </p>
+      ))}
+    </div>
+  );
+};
+
+const Total = ({ total }: { total: number }) => {
+  return <p>Number of exercises {total}</p>;
+};
+
+const App = () => {
+  const courseName = "Half Stack application development";
+  const courseParts = [
+    {
+      name: "Fundamentals",
+      exerciseCount: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exerciseCount: 7,
+    },
+    {
+      name: "Deeper type usage",
+      exerciseCount: 14,
+    },
+  ];
+
+  const totalExercises = courseParts.reduce(
+    (sum, part) => sum + part.exerciseCount,
+    0
+  );
+
+  return (
+    <div>
+      <Header name={courseName} />
+      <Content parts={courseParts} />
+      <Total total={totalExercises} />
+    </div>
+  );
+};
+
+export default App;
