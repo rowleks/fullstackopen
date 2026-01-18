47672e834ac7fdab8ed7a7b01911276f41052f62
Rowland Momoh <momohrowland@gmail.com>
2026-01-18 17:23:55 +0100

    Update types on patientor frontend

--- part9/patientor/patientor-frontend/src/types.ts
+++ part9/patientor/patientor-frontend/src/types.ts
@@ -1,4 +1,4 @@
-export interface Diagnosis {
+export interface Diagnoses {
   code: string;
   name: string;
   latin?: string;
@@ -7,16 +7,67 @@ export interface Diagnosis {
 export enum Gender {
   Male = "male",
   Female = "female",
-  Other = "other"
+  Other = "other",
 }
 
-export interface Patient {
+//eslint-disable-next-line @typescript-eslint/no-explicit-any
+type UnionOmit<T, K extends keyof any> = T extends unknown ? Omit<T, K> : never;
+
+interface BaseEntry {
   id: string;
-  name: string;
-  occupation: string;
-  gender: Gender;
-  ssn?: string;
-  dateOfBirth?: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnoses["code"]>;
 }
 
-export type PatientFormValues = Omit<Patient, "id" | "entries">;
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: {
+    date: string;
+    criteria: string;
+  };
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
+interface PatientBase {
+  name: string;
+  dateOfBirth: string;
+  gender: Gender;
+  occupation: string;
+  ssn: string;
+}
+
+export interface Patient extends PatientBase {
+  id: string;
+  entries: Entry[];
+}
+
+export interface PatientFormValues extends PatientBase {
+  entries: Array<UnionOmit<Entry, "id">>;
+}
